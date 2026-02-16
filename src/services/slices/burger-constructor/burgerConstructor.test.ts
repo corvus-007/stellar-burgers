4a497f81755fd71366bdab853b52a1d5b98b10c2
mrnobody <corvus007@yandex.ru>
2026-02-16 09:19:29 +0300

    Добавляет тесты для редьюсера `burgerConstructor` на удаление ингредиента

--- src/services/slices/burger-constructor/burgerConstructor.test.ts
+++ src/services/slices/burger-constructor/burgerConstructor.test.ts
@@ -1,13 +1,15 @@
 import {
   addIngredient,
   burgerConstructorSlice,
+  removeIngredient,
   setBun,
   TBurgerConstructorState
 } from './burgerConstructorSlice';
 import { TIngredient } from '@utils-types';
+import { nanoid } from '@reduxjs/toolkit';
 
 describe('Проверяют редьюсер слайса burgerConstructor', () => {
-  test('добаление булочки', () => {
+  test('добавление булочки', () => {
     const initialState: TBurgerConstructorState = {
       constructorItems: {
         bun: null,
@@ -16,7 +18,7 @@ describe('Проверяют редьюсер слайса burgerConstructor', (
       orderRequest: false,
       orderModalData: null
     };
-    const expectedBun: TIngredient = {
+    const bun: TIngredient = {
       _id: '643d69a5c3f7b9001cfa093c',
       name: 'Краторная булка N-200i',
       type: 'bun',
@@ -29,15 +31,8 @@ describe('Проверяют редьюсер слайса burgerConstructor', (
       image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
       image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png'
     };
-
-    const newState = burgerConstructorSlice.reducer(
-      initialState,
-      setBun(expectedBun)
-    );
-
-    const {
-      constructorItems: { bun }
-    } = newState;
+    const newState = burgerConstructorSlice.reducer(initialState, setBun(bun));
+    const expectedBun = newState.constructorItems.bun;
 
     expect(bun).toEqual(expectedBun);
   });
@@ -64,20 +59,46 @@ describe('Проверяют редьюсер слайса burgerConstructor', (
       image_mobile: 'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
       image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png'
     };
-
     const newState = burgerConstructorSlice.reducer(
       initialState,
       addIngredient(ingredient)
     );
-
-    const {
-      constructorItems: {
-        ingredients: [expectedIngredient]
-      }
-    } = newState;
+    const expectedIngredient = newState.constructorItems.ingredients[0];
 
     expect(expectedIngredient).toHaveProperty('id');
     const { id, ...rest } = expectedIngredient;
     expect(rest).toEqual(ingredient);
+  });
+
+  test('удаление ингредиента', () => {
+    const ingredient = {
+      id: nanoid(),
+      _id: '643d69a5c3f7b9001cfa0941',
+      name: 'Биокотлета из марсианской Магнолии',
+      type: 'main',
+      proteins: 420,
+      fat: 142,
+      carbohydrates: 242,
+      calories: 4242,
+      price: 424,
+      image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+      image_mobile: 'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+      image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png'
+    };
+    const initialState: TBurgerConstructorState = {
+      constructorItems: {
+        bun: null,
+        ingredients: [ingredient]
+      },
+      orderRequest: false,
+      orderModalData: null
+    };
+    const newState = burgerConstructorSlice.reducer(
+      initialState,
+      removeIngredient(ingredient)
+    );
+    const ingredients = newState.constructorItems.ingredients;
+
+    expect(ingredients).toHaveLength(0);
   });
 });
