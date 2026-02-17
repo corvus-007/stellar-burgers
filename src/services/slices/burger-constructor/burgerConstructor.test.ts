e9d7a9fa79d07a4af7d902b8ac1eb870b7593158
mrnobody <corvus007@yandex.ru>
2026-02-17 21:50:07 +0300

    Добавляет тесты для редьюсера `burgerConstructor` на очистку ингредиентов

--- src/services/slices/burger-constructor/burgerConstructor.test.ts
+++ src/services/slices/burger-constructor/burgerConstructor.test.ts
@@ -1,6 +1,7 @@
 import {
   addIngredient,
   burgerConstructorSlice,
+  clearConstructorItems,
   moveDownIngredient,
   moveUpIngredient,
   removeIngredient,
@@ -102,6 +103,61 @@ describe('Проверяют редьюсер слайса burgerConstructor', (
     const ingredients = newState.constructorItems.ingredients;
 
     expect(ingredients).toHaveLength(0);
+  });
+
+  test('очистка ингредиентов', () => {
+    const initialState: TBurgerConstructorState = {
+      constructorItems: {
+        bun: {
+          _id: '643d69a5c3f7b9001cfa093c',
+          name: 'Краторная булка N-200i',
+          type: 'bun',
+          proteins: 80,
+          fat: 24,
+          carbohydrates: 53,
+          calories: 420,
+          price: 1255,
+          image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+          image_mobile:
+            'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+          image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png'
+        },
+        ingredients: [
+          {
+            id: nanoid(),
+            _id: '643d69a5c3f7b9001cfa0941',
+            name: 'Биокотлета из марсианской Магнолии',
+            type: 'main',
+            proteins: 420,
+            fat: 142,
+            carbohydrates: 242,
+            calories: 4242,
+            price: 424,
+            image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+            image_mobile:
+              'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+            image_large:
+              'https://code.s3.yandex.net/react/code/meat-01-large.png'
+          }
+        ]
+      },
+      orderRequest: false,
+      orderModalData: null
+    };
+    const expectedState: TBurgerConstructorState = {
+      constructorItems: {
+        bun: null,
+        ingredients: []
+      },
+      orderRequest: false,
+      orderModalData: null
+    };
+    const newState = burgerConstructorSlice.reducer(
+      initialState,
+      clearConstructorItems()
+    );
+
+    expect(newState).toEqual(expectedState);
   });
 
   describe('перемещение ингредиентов', () => {
