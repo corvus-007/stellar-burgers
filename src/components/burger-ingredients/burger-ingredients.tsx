218837ea5d544ea75aeb69442fd82632a06df6dc
mrnobody <corvus007@yandex.ru>
2026-02-22 19:13:18 +0300

    доработка

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect, FC } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { TTabMode } from '@utils-types';
-import { BurgerIngredientsUI } from '../ui/burger-ingredients';
+import { BurgerIngredientsUI } from '@ui';
 import { useSelector } from '../../services/store';
 import {
   getBuns,
@@ -11,7 +11,6 @@ import {
 } from '../../services/slices/burger-ingredients/burgerIngredientsSlice';
 
 export const BurgerIngredients: FC = () => {
-  /** TODO: взять переменные из стора */
   const buns = useSelector(getBuns);
   const mains = useSelector(getMains);
   const sauces = useSelector(getSauces);
